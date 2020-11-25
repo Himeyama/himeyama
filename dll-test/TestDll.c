50a0549a2c105d96fb038faa0c7b78d9a1663341
ひかり <39254183+Himeyama@users.noreply.github.com>
2020-11-26 02:15:20 +0900

    Add files via upload

--- dll-test/TestDll.c
+++ dll-test/TestDll.c
@@ -1,14 +1,5 @@
-// #include "TestDll.h"
-// #include <windows.h>
+#include <windows.h>
 
-// int main(void){
-//     return 0;
-// }
-
-// extern "C" {
-//     BOOL func();
-// }
-
-int func(){
-    return 1;
+BOOL func(){
+    return TRUE;
 }
