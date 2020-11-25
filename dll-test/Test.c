6301e41c5b633d3a221d70c562b2d4bc22b6882c
ひかり <39254183+Himeyama@users.noreply.github.com>
2020-11-26 02:05:25 +0900

    Add files via upload

---
+++ dll-test/Test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "TestDll.h"
+
+int main(void){
+    printf("%d\n", func());
+
+    return 0;
+}
---
+++ dll-test/TestDll.c
@@ -0,0 +1,14 @@
+// #include "TestDll.h"
+// #include <windows.h>
+
+// int main(void){
+//     return 0;
+// }
+
+// extern "C" {
+//     BOOL func();
+// }
+
+int func(){
+    return 1;
+}
