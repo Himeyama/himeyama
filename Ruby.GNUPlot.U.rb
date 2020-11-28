ef6d7ed48cbcb6fd17040912f9ab2e74a51ec0db
ひかり <39254183+Himeyama@users.noreply.github.com>
2020-11-29 02:42:12 +0900

    Update Ruby.GNUPlot.U.rb

--- Ruby.GNUPlot.U.rb
+++ Ruby.GNUPlot.U.rb
@@ -4,7 +4,7 @@ include Numo
 
 class GNUPlot
     def initialize(
-    plt = nil, 
+    plt = [], 
     title = nil,
     file_name = nil, 
     font_family = "Alial", 
@@ -49,20 +49,82 @@ class GNUPlot
         end
     end
     
+    def add_data(dat)
+        @plt += dat.get
+    end
+    
+    def data=(data)
+        data.each do |d|
+            add_data(d)
+        end
+    end
+    
     attr_accessor :grid, :ratio, :text_color, :plt, :font_family, :font_size, :light_gray, :key_font_size, :file_name, :xlabel, :ylabel, :title
 end
 
+class GNUPlotData
+    def initialize(x, y) 
+        @x = x
+        @y = y
+        @line = true
+        @point = true
+        @width = 1
+        @title = nil
+    end
+    
+    def get
+        opt = {}
+        if line
+            opt["w"] = "l"
+            opt["lw"] = @width
+        end
+        if point
+            opt["w"] = "p"
+            opt["pt"] = @pt
+        end
+        opt["w"] = "lp" if line && point
+        opt["t"] = @title if @title
+        opt["lc"] = "rgb 0x#{@color}" if @color
+        
+        [@x, @y, opt]
+    end
+    
+    def width(w)
+        @width = w
+        self
+    end
+    
+    def color(c)
+        @color = c
+        self
+    end
+    
+    def pt(pt)
+        @pt = pt
+        self
+    end
+    
+    attr_accessor :x, :y, :line, :point, :title
+end
+
+colors = ["2196F3", "FF9800", "009688", "f44336"]
+
+
 x = Numo::Int32[-10..10] / 10.0
-y = x 
+y1 = x 
 y2 = x ** 2
 y3 = x ** 3
 y4 = x ** 4
 
 plt = GNUPlot.new
-plt.plt = [x, y, {w: "lp", pt: 5, lw: 3, lc: "rgb 0x#{colors[0]}", t: "x"},
-                x, y2, {w: "lp", pt: 7, lw: 3, lc: "rgb 0x#{colors[1]}", dt: "(5, 1)", t: "x^2"},
-                x, y3, {w: "lp", pt: 5, lw: 3, lc: "rgb 0x#{colors[2]}", dt: "(3, 1)", t: "x^3"},
-                x, y4, {w: "lp", pt: 7, lw: 3, lc: "rgb 0x#{colors[3]}", t: "x^4"}]
+
+d1 = GNUPlotData.new(x, y1).width(3).color(colors[0]).pt(5)
+d2 = GNUPlotData.new(x, y2).width(3).color(colors[1]).pt(7)
+d3 = GNUPlotData.new(x, y3).width(3).color(colors[2]).pt(5)
+d4 = GNUPlotData.new(x, y4).width(3).color(colors[3]).pt(7)
+
+plt.data = d1, d2, d3, d4
+
 plt.xlabel = "横軸"
 plt.ylabel = "縦軸"
 plt.title = "タイトル"
